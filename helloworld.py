58f1a45e4640bde6d850f3072ba5ce2bc50dc292
Alexander Ryan Crosby <acrosby6@mail.valenciacollege.edu>
2016-01-20 18:36:09 -0500

    Alex Crosby Language Program
    My robot butler asks me what language I would like to speak in :)

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,24 @@
-# replace the contents of this comment with your full name
+# Alexander Ryan Crosby
+#This is the intro for my Robo Butler language request
+print('Good day to you master!')
+print('what language would you like to converse in today?')
+print('1. Spanish')
+print('2. French')
+print('3. German')
+language=input() # Making input language isn't really necessary but I wanted to
+                 # simplify it for myself
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
+if language == 'spanish':
+    print('Hola!')
+elif language == 'french':
+    print('Bonjour!')
+elif language == 'german':
+    print('Hallo!')
+elif language != 'spanish' or 'french' or 'german': # This is just so that it
+    print('That is not a correct input...')         # just doesn't close out
+    print('Are you quite alright master?')          # when there is an incorrect
+                                                    # input. Not needed though
 
-# make sure that your code contains comments explaining your logic!
-
+import sys  # Just so that the prgram doesn't immediately close when it finishes
+input()     # executing. Want to at least see the response based on my input
+sys.exit
